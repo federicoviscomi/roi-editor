cb113ff55242a21c72ae48645e3e92b65938bc19
fviscomi <federicoviscomi@gmail.com>
2021-03-02 11:22:13 +0000

    first commit

---
+++ src/panels/PanelList.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import styles from './styles';
+
+class PanelList extends Component {
+    render() {
+        let {object, objectComponent, id} = this.props;
+
+        return (
+            <div style={{...styles.propertyPanel}}>
+                {objectComponent.panels.map((Panel, i) => <Panel key={i} id={id} {...this.props} />)}
+            </div>
+        );
+    }
+};
+
+export default PanelList;
